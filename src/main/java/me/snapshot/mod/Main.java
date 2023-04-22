0ab7d993c3e6c2c58d8a93f9f749f1273bcd0aaf
Mqrshie <spys23@hotmail.com>
2023-04-22 14:01:25 +0800

    i hate fabric and mc sometimes

---
+++ src/main/java/me/snapshot/mod/Main.java
@@ -0,0 +1,26 @@
+package me.snapshot.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "wa";
+    public static final String MOD_NAME = "23w16a";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
